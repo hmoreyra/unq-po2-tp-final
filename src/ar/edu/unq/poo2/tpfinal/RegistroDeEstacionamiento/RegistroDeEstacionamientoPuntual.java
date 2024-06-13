33cc2e9c4923486f709459f016a06c1dbd77a3d8
Jorge Terradillos <jorgenterradillos@gmail.com>
2024-06-13 18:03:30 -0300

    agrego registro de estacionamiento puntual

---
+++ src/ar/edu/unq/poo2/tpfinal/RegistroDeEstacionamiento/RegistroDeEstacionamientoPuntual.java
@@ -0,0 +1,25 @@
+package ar.edu.unq.poo2.tpfinal.RegistroDeEstacionamiento;
+
+import java.time.LocalDateTime;
+
+import ar.edu.unq.poo2.tpfinal.ZonaDeEstacionamiento.*;
+
+public class RegistroDeEstacionamientoPuntual extends RegistroDeEstacionamiento{
+	
+	int horasEstacionamiento;
+	
+	public RegistroDeEstacionamientoPuntual(String patente, int horasEstacionamiento, LocalDateTime fechaYHoraDeInicio, ZonaDeEstacionamiento zonaDeEstacionamiento) {
+		this.patente = patente;
+		this.horasEstacionamiento = horasEstacionamiento;
+		this.fechaYHoraDeInicio = fechaYHoraDeInicio;
+		this.zonaDeEstacionamiento = zonaDeEstacionamiento;
+		this.vigencia = true;
+	}
+	
+	public int getHorasEstacionamiento() {
+		return this.horasEstacionamiento;
+	}
+	
+		
+
+}
