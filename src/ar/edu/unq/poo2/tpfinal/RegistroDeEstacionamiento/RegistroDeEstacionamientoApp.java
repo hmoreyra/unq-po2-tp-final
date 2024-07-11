fa84aed910646da3b2fc60db3dbb1dc93ea145ad
Galarza Matias <matias.galarza.unq@gmail.com>
2024-07-11 02:20:22 -0300

    Fix merge conflicts

---
+++ src/ar/edu/unq/poo2/tpfinal/RegistroDeEstacionamiento/RegistroDeEstacionamientoApp.java
@@ -0,0 +1,10 @@
+package ar.edu.unq.poo2.tpfinal.RegistroDeEstacionamiento;
+
+public class RegistroDeEstacionamientoApp extends RegistroDeEstacionamiento {
+
+	public int getNumeroDeCelular() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
